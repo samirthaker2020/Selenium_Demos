f1af604ed683cc2a5b3e1d21bbbc128ae92c39a1
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2020-12-22 00:22:59 -0500

    Initial commit

---
+++ src/main/java/Config.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Config {
+
+
+}
---
+++ src/main/java/OpenBrowser.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
+
+public class OpenBrowser {
+
+    public static void main(String[] args) {
+// System Property for Chrome Driver
+        System.setProperty("webdriver.chrome.driver", "I:\\Appium Projects\\Selenium_demos\\src\\chromedriver_win32\\chromedriver.exe");
+
+
+        WebDriver driver=new ChromeDriver();
+        // Launch Website
+        driver.navigate().to("http://www.javatpoint.com/");
+
+        //Maximize the browser
+        driver.manage().window().maximize();
+
+        //Scroll down the webpage by 5000 pixels
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("scrollBy(0, 5000)");
+
+        // Click on the Search button
+        driver.findElement(By.linkText("Core Java")).click();
+
+    }
+
+}
